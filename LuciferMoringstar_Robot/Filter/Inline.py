4053f05294134d198b81b8e24b0773473f63c531
George Bobby <georgebobbyawesome@gmail.com>
2022-06-15 15:37:51 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Join Channel', url=f'{PROCHANNEL}')
+        InlineKeyboardButton('Join Channel', url=f'{TUTORIAL}')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
